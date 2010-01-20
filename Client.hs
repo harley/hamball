04401b7b5db379c94dbeb5bbbee066280c3ff001
Harley Trung <harley.trung@yale.edu>
2010-01-20 00:54:44 -0500

    more cleanup

--- Client.hs
+++ Client.hs
@@ -1,29 +1,38 @@
 module Main where
 
-import Network
-import RunGame
-import FRP.Yampa
-import Terrain
-import Common
-import Object
-import Net
+import Network (PortID(PortNumber), connectTo, withSocketsDo)
 import System (getArgs)
-import Control.Monad
-import System.IO
+import Control.Monad (when)
 
+import FRP.Yampa ((>>>), arr) -- NOTE that this is our modified YAMPA
+import RunGame (glInit, runGame, game)
+import Common (CSMsg'(CSMsgJoin))
+import Object (serverObject, scoreboard, terrain0, renderObsObjState)
+import Net (sendCSMsg)
+
+-- Client triggers the game with:
+-- ./Client <player-name> <host-name>
 main :: IO ()
-main = withSocketsDo $ do
-    serverHost <- readFile hostName
-    print serverHost
-
+main = withSocketsDo $ do -- withSocketsDo is only needed for Windows platform, harmless on others
     args <- getArgs
-    when (null args) $ error "Please specify your player name"
-    let playerNameStr = head args
+    when (length args < 1) $ error "Wrong syntax.  Syntax: ./Client <play-name> <host-name>"
+    let (playerName:serverHost:_) = args
 
+    print ("Connecting player " ++ playerName ++ " to " ++ serverHost)
+
+    -- Ask server to connect
     handle <- connectTo serverHost (PortNumber 4444)
-    sendCSMsg handle $ (-1, CSMsgJoin playerNameStr)
+
+    -- Tell server that this player is joining
+    sendCSMsg handle $ (-1, CSMsgJoin playerName)
+
+    -- Prepare some OpenGL intialization and windows management
     glInit
-    let initialObjs = [serverObject playerNameStr, scoreboard, terrain0]
+
+    let initialObjs = [serverObject playerName, scoreboard, terrain0]
+
+    -- TODO: Explain runGame
     runGame (Just handle) (game initialObjs >>> (arr (\(ooss,msgs) -> (renderObsObjStates ooss, sendNetworkMsgs handle msgs))))
   where renderObsObjStates = foldl (\io oos -> io >> renderObsObjState oos) (return ())
         sendNetworkMsgs h = foldl (\io msg -> io >> sendCSMsg h msg) (return ())
+
--- Net.hs
+++ Net.hs
@@ -16,7 +16,7 @@ instance Stringifiable Hit where
                   (show $ hitLaserID h) ++ delim ++
                   (show $ hitStr h)
         where delim = ";"
-                         
+
     destringify s = let untildelim = span (/= ';')
                         (p1,s1) = untildelim s
                         (p2,s2) = untildelim $ drop 1 s1
@@ -27,23 +27,23 @@ instance Stringifiable Hit where
                              hitStr = read $ drop 1 s3}
 
 instance Stringifiable Obj where
-    
+
     stringify (PlayerObj p) = "player:" ++ (stringify p)
     stringify (LaserObj l) = "laser:" ++ (stringify l)
-    
+
     destringify s = let (p1,s1) = span (/= ':') s
                     in case p1 of
                           "player" -> PlayerObj $ destringify $ drop 1 s1
                           "laser" -> LaserObj $ destringify $ drop 1 s1
 
 instance Stringifiable SCMsg' where
-   
+
     stringify (SCMsgInitialize p) = "initialize:" ++ (stringify p)
     stringify (SCMsgPlayer p) = "player:" ++ (stringify p)
     stringify (SCMsgHit h) = "hit:" ++ (stringify h)
     stringify (SCMsgSpawn obj) = "spawn:" ++ (stringify obj)
     stringify (SCMsgFrag p) = "frag:" ++ (stringify p)
-          
+
     destringify s = let untildelim = span (/= ':')
                         (p1,s1) = untildelim s
                     in case p1 of
@@ -52,7 +52,7 @@ instance Stringifiable SCMsg' where
                           "hit"        -> SCMsgHit $ destringify $ drop 1 s1
                           "spawn"      -> SCMsgSpawn $ destringify $ drop 1 s1
                           "frag"       -> SCMsgFrag $ destringify $ drop 1 s1
-                        
+
 instance Stringifiable CSMsg' where
 
     stringify (CSMsgPlayer p) = "player:" ++ (stringify p)
@@ -62,7 +62,7 @@ instance Stringifiable CSMsg' where
     stringify (CSMsgDeath ident) = "death:" ++ (show ident)
     stringify (CSMsgJoin name) = "join:" ++ name
     stringify (CSMsgExit) = "exit"
-          
+
     destringify s = let untildelim = span (/= ':')
                         (p1,s1) = untildelim s
                     in case p1 of
@@ -73,20 +73,20 @@ instance Stringifiable CSMsg' where
                           "death" -> CSMsgDeath $ read $ drop 1 s1
                           "join" -> CSMsgJoin $ drop 1 s1
                           "exit" -> CSMsgExit
-                                  
+
 instance Stringifiable SCMsg where
-    
+
     stringify (ident,scmsg') = (show ident) ++ ":" ++ (stringify scmsg')
     destringify s = let (p1,s1) = span (/= ':') s
                     in (read p1, destringify $ drop 1 s1)
-                    
+
 instance Stringifiable CSMsg where
-    
+
     stringify (ident,csmsg') = (show ident) ++ ":" ++ (stringify csmsg')
-    
+
     destringify s = let (p1,s1) = span (/= ':') s
                     in (read p1, destringify $ drop 1 s1)
-    
+
 fetchSCMsg :: ReactChan GameInput -> Handle -> IO ()
 fetchSCMsg rch h = do
     ln <- hGetLine h
@@ -95,9 +95,10 @@ fetchSCMsg rch h = do
                 (_,SCMsgHit h) -> True
                 _ -> False
     react rch (\gi -> gi {message = destringify ln}) False
-    
+
+-- Send msg from Client to Server
 sendCSMsg :: Handle -> CSMsg -> IO ()
 sendCSMsg h msg = do
     hPutStrLn h (stringify msg)
-    hFlush h
+    --hFlush h -- I dont think we need to flush manually here. Harley
 
